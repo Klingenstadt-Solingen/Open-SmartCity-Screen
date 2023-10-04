7ea169a9fa14113ea3e0529e7367e0d8a6d53f22
Jakob Windoffer <j.windoffer@nedeco.de>
2023-10-04 12:50:46 +0200

    fix: linting error

--- renderer/common/layouts/diashow/Diashow.tsx
+++ renderer/common/layouts/diashow/Diashow.tsx
@@ -2,14 +2,5 @@ import React from 'react'
 import ImageTile from './tiles/ImageTile'
 
 export default function Diashow(): React.JSX.Element {
-  return (
-    <ImageTile
-      dimensionForPdf={{
-        docCptSize: 'w-[100vw]',
-        pdfScale: 1.3
-      }}
-      setCenter={undefined}
-      isOpen={undefined}
-    ></ImageTile>
-  )
+  return <ImageTile></ImageTile>
 }
