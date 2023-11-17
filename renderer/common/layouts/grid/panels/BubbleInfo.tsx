52af607e80b0a61caf8d5c10d2b3c5059c0f8a9a
Jakob Windoffer <j.windoffer@nedeco.de>
2023-11-17 16:51:36 +0100

    feat(map): added detailed map popups and public traffic

--- renderer/common/layouts/grid/panels/BubbleInfo.tsx
+++ renderer/common/layouts/grid/panels/BubbleInfo.tsx
@@ -1,116 +1,198 @@
 import React from 'react'
+import QRCode from 'react-qr-code'
+import Route from '../../../icons/Route'
 
-interface Props {
-  information: {
-    address: string
-    city: string
-    district: string
-    name: string
-    zip: string
-    homePage: string
-    tel: string
-    symbolName: string
-  }
+export default function BubbleInfo(props: any): React.JSX.Element {
+  return (
+    <>
+      <MainInfo information={props.information}></MainInfo>
+      {props.information?.poi?.details?.find((el) => el.title === 'Sie Sind Hier') ===
+        undefined && (
+        <button
+          onMouseDown={() => {
+            props.tripTo(props?.information?.coordinates[0], props?.information?.coordinates[1])
+          }}
+          className="w-full pl-[2rem] left-[-2rem] h-[12rem] rounded-3xl text-on-primary-color text-xl relative bg-primary-color shadow-xl shadow-gray-500 flex justify-center items-center"
+        >
+          <div className="h-full w-full flex py-2 px-2">
+            <Route></Route>
+          </div>
+        </button>
+      )}
+    </>
+  )
 }
 
-export default function BubbleInfo(props: Props): React.JSX.Element {
-  let iconName: string
-  switch (props.information?.symbolName) {
-    case 'XE_Unterkunft':
-      iconName = 'hotel.svg'
-      break
-    case 'XE_Autoverkehr_ECarLadestation':
-      iconName = 'gasstation.svg'
-      break
-    case 'XE_Behoerde':
-      iconName = 'cityhall.svg'
-      break
-    case 'XE_TechnischesBauwerk_Freifunk':
-      iconName = 'wifi2.svg'
-      break
-    case 'XE_Autoverkehr_Baustelle':
-      iconName = 'construction.svg'
-      break
-    case 'XE_Tourismus_Touristische_Infrastruktur':
-      iconName = 'maproute.svg'
-      break
+function MainInfo(props: any): React.JSX.Element {
+  function getAttribute(attribute: string) {
+    return props.information?.poi?.details?.find((el) => el.title === attribute)
   }
 
   return (
-    <div className="w-screen max-w-lg flex-auto overflow-hidden rounded-3xl bg-secondary-color text-2xl leading-6 shadow-lg ring-1 ring-gray-900/5">
-      <div className="p-6">
-        <div className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
-          <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-            {iconName && <img src={'/images/svg/' + iconName} width={25} />}
-          </div>
-          <div className="w-full text-left">
-            <a href="#" className="font-semibold text-gray-900 text-3xl">
-              {props.information?.name}
-              <span className="absolute inset-0"></span>
-            </a>
-            <p className="mt-1 text-gray-600"></p>
-          </div>
+    props.information?.poi?.details && (
+      <div
+        className="overflow-hidden text-left gap-4 p-4 grid grid-flow-row gap-x-5 box-border rounded-3xl bg-secondary-color text-on-secondary-color shadow-xl shadow-gray-500 z-50"
+        style={{ gridTemplateColumns: 'minmax(0, 1fr) minmax(0,1fr)' }}
+      >
+        {getAttribute('Sie Sind Hier') && (
+          <InfoLine content={getAttribute('Sie Sind Hier')} name="Sie Sind Hier"></InfoLine>
+        )}
+        {getAttribute('Name') && <InfoLine content={getAttribute('Name')} name="Name"></InfoLine>}
+        {getAttribute('Bezeichnung') && (
+          <InfoLine content={getAttribute('Bezeichnung')} name="Bezeichnung"></InfoLine>
+        )}
+
+        {/* ------------------------- */}
+        {getAttribute('Baustelle') && (
+          <InfoLine content={getAttribute('Baustelle')} name="Baustelle"></InfoLine>
+        )}
+        {getAttribute('Baustatus') && (
+          <InfoLine content={getAttribute('Baustatus')} name="Baustatus"></InfoLine>
+        )}
+        {getAttribute('Baubeginn') && (
+          <InfoLine content={getAttribute('Baubeginn')} name="Baubeginn"></InfoLine>
+        )}
+        {getAttribute('Bauende') && (
+          <InfoLine content={getAttribute('Bauende')} name="Bauende"></InfoLine>
+        )}
+        {/* ------------------------- */}
+        {getAttribute('Onlinestatus') && (
+          <InfoLine content={getAttribute('Onlinestatus')} name="Onlinestatus"></InfoLine>
+        )}
+
+        {/* ------------------------- */}
+        {getAttribute('Angebot') && (
+          <InfoLine content={getAttribute('Angebot')} name="Angebot"></InfoLine>
+        )}
+        {/* ------------------------- */}
+        {getAttribute('Parkplatztyp') && (
+          <InfoLine content={getAttribute('Parkplatztyp')} name="Parkplatztyp"></InfoLine>
+        )}
+        {getAttribute('Leistung') && (
+          <InfoLine content={getAttribute('Leistung')} name="Leistung"></InfoLine>
+        )}
+        {getAttribute('Standard') && (
+          <InfoLine content={getAttribute('Standard')} name="Standard"></InfoLine>
+        )}
+        {getAttribute('Zugänglich') && (
+          <InfoLine content={getAttribute('Zugänglich')} name="Zugänglich"></InfoLine>
+        )}
+        {getAttribute('Betreiber') && (
+          <InfoLine content={getAttribute('Betreiber')} name="Betreiber"></InfoLine>
+        )}
+        {getAttribute('Freie Plätze') && (
+          <InfoLine content={getAttribute('Freie Plätze')} name="Freie Plätze"></InfoLine>
+        )}
+
+        {/* ------------------------- */}
+        {getAttribute('Branche') && (
+          <InfoLine content={getAttribute('Branche')} name="Branche"></InfoLine>
+        )}
+        {getAttribute('Versand') && (
+          <InfoLine content={getAttribute('Versand')} name="Versand"></InfoLine>
+        )}
+        {getAttribute('Lieferung') && (
+          <InfoLine content={getAttribute('Lieferung')} name="Lieferung"></InfoLine>
+        )}
+        {getAttribute('Abholung') && (
+          <InfoLine content={getAttribute('Abholung')} name="Abholung"></InfoLine>
+        )}
+        {getAttribute('Hinweise') && (
+          <InfoLine content={getAttribute('Hinweise')} name="Hinweise"></InfoLine>
+        )}
+        {getAttribute('Nachhaltigkeit') && (
+          <InfoLine content={getAttribute('Nachhaltigkeit')} name="Nachhaltigkeit"></InfoLine>
+        )}
+        {getAttribute('Zertifikate') && (
+          <InfoLine content={getAttribute('Zertifikate')} name="Zertifikate"></InfoLine>
+        )}
+        {/* ------------------------- */}
+
+        {getAttribute('Unterkunftsart') && (
+          <InfoLine content={getAttribute('Unterkunftsart')} name="Unterkunftsart"></InfoLine>
+        )}
+
+        {/* ------------------------- */}
+
+        {getAttribute('Telefon') && (
+          <InfoLine content={getAttribute('Telefon')} name="Telefon"></InfoLine>
+        )}
+        {getAttribute('Email') && (
+          <InfoLine content={getAttribute('Email')} name="Email"></InfoLine>
+        )}
+        {getAttribute('Website') && (
+          <InfoLine content={getAttribute('Website')} name="Website"></InfoLine>
+        )}
+        {getAttribute('Homepage') && (
+          <InfoLine content={getAttribute('Homepage')} name="Homepage"></InfoLine>
+        )}
+      </div>
+    )
+  )
+}
+
+function InfoLine(props: any): React.JSX.Element {
+  if (props.content.iconName === 'telefonicon') {
+    props.content.iconName = 'mobileicon'
+  }
+
+  return (
+    <div
+      className="grid justify-center items-start"
+      style={{
+        gridTemplateColumns: 'minmax(0, 1fr) minmax(0, 4fr)',
+        overflow: 'hidden',
+        overflowWrap: 'normal'
+      }}
+    >
+      {props.content.iconName && (
+        <img
+          src={props.content.iconPath + '/' + props.content.iconName + props.content.iconMimetype}
+        />
+      )}
+      {!props.content.iconName && <div></div>}
+      <div className="text-xl font-bold">
+        <div className="text-xl mt-1">
+          {props.name}
+          {props.name !== 'Sie Sind Hier' ? ':' : ''}
         </div>
-        <div className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
-          <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg"></div>
-          <div className="w-full text-left">
-            <a href="#" className="font-semibold text-gray-900">
-              Adresse:
-              <span className="absolute inset-0"></span>
-            </a>
-            <p className="mt-1 text-gray-600">{props.information?.address}</p>
-          </div>
-        </div>
-        <div className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
-          <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg"></div>
-          <div className="w-full text-left">
-            <a href="#" className="font-semibold text-gray-900">
-              Stadtteil:
-              <span className="absolute inset-0"></span>
-            </a>
-            <p className="mt-1 text-gray-600">{props.information?.district}</p>
-          </div>
-        </div>
-        <div className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
-          <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg"></div>
-          <div className="w-full text-left">
-            <a href="#" className="font-semibold text-gray-900">
-              Postleitzahl:
-              <span className="absolute inset-0"></span>
-            </a>
-            <p className="mt-1 text-gray-600">{props.information?.zip}</p>
-          </div>
-        </div>
-        <div className="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
-          <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg"></div>
-          <div className="w-full text-left">
-            <a href="#" className="font-semibold text-gray-900">
-              Stadt:
-              <span className="absolute inset-0"></span>
-            </a>
-            <p className="mt-1 text-gray-600">{props.information?.city}</p>
+        <div
+          className="font-normal text-lg line-clamp-2 max-w-full"
+          onMouseDown={(event) => event.currentTarget.classList.remove('line-clamp-2')}
+        >
+          {props.content.value}
+
+          <div className="w-full flex justify-center items-end">
+            {props.name === 'Email' && <QR link={`mailto:${props.content.value}`} />}
+
+            {props.name === 'Telefon' && <QR link={`tel:${props.content.value}`} />}
+
+            {(props.name === 'Website' || props.name === 'Homepage') && (
+              <QR link={`${props.content.value}`} />
+            )}
           </div>
         </div>
       </div>
-      {(props.information?.homePage || props.information?.tel) && (
-        <div className="grid grid-cols-1 divide-x divide-gray-900/5 bg-primary-color break-all p-6 leading-8">
-          {props.information?.homePage && (
-            <>
-              <a href="#" className="flex text-left gap-x-2.5 font-semibold text-on-primary">
-                <img src="/images/svg/internet.svg" width={25} className="ml-5 mr-3" />
-                {props.information?.homePage}
-              </a>
-              <br />
-            </>
-          )}
-          {props.information?.tel && (
-            <a className="flex text-left gap-x-2.5 font-semibold text-on-primary">
-              <img src="/images/svg/telephone.svg" width={25} className="ml-5 mr-3 -mt-1" />
-              {props.information?.tel}
-            </a>
-          )}
-        </div>
-      )}
+    </div>
+  )
+}
+
+function QR(props: any): React.JSX.Element {
+  return (
+    <div
+      className="w-1/5 transition-all duration-app-speed flex"
+      onMouseDown={(event) => {
+        if (event.currentTarget.classList.contains('w-full')) {
+          event.currentTarget.classList.remove('w-full')
+          event.currentTarget.classList.add('w-1/5')
+        } else {
+          event.currentTarget.classList.remove('w-1/5')
+
+          event.currentTarget.classList.add('w-full')
+        }
+      }}
+    >
+      <QRCode className="w-full h-min" value={props.link}></QRCode>
     </div>
   )
 }
--- renderer/common/layouts/grid/panels/MapPanel.tsx
+++ renderer/common/layouts/grid/panels/MapPanel.tsx
@@ -16,6 +16,17 @@ import { POI } from '../../../../models/poi'
 import Overlay from 'ol/Overlay.js'
 import BubbleInfo from './BubbleInfo'
 
+import Parse from 'parse'
+
+import * as olProj from 'ol/proj.js'
+import * as olGeom from 'ol/geom'
+import * as olSource from 'ol/source'
+import * as olLayer from 'ol/layer'
+import * as olStyle from 'ol/style.js'
+import * as ol from 'ol'
+import { environment } from '../../../../environment'
+import Close from '../../../icons/Close'
+
 declare global {
   interface Window {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -24,20 +35,37 @@ declare global {
 }
 
 function styleWfs(feature) {
-  const icon = new Style({
-    image: new Icon({
-      src:
-        feature.values_.symbolPath +
-        '/' +
-        feature.values_.symbolName +
-        feature.values_.symbolMimetype,
+  if (feature?.values_?.poi?.details?.find((el) => el.title === 'Onlinestatus')) {
+    const icon = new Style({
+      image: new Icon({
+        src:
+          feature.values_.poi?.details.find((el) => el.title === 'Onlinestatus').symbolPath +
+          '/' +
+          feature.values_.poi?.details.find((el) => el.title === 'Onlinestatus').symbolName +
+          feature.values_.poi?.details.find((el) => el.title === 'Onlinestatus').symbolMimetype,
 
-      scale: 0.85,
-      opacity: 1
+        scale: 1.1,
+        opacity: 1
+      })
     })
-  })
 
-  return [icon]
+    return [icon]
+  } else {
+    const icon = new Style({
+      image: new Icon({
+        src:
+          feature.values_.symbolPath +
+          '/' +
+          feature.values_.symbolName +
+          feature.values_.symbolMimetype,
+
+        scale: 1.3,
+        opacity: 1
+      })
+    })
+
+    return [icon]
+  }
 }
 
 function mapToLayers(pois: POI[], categories: PoiCategory[]) {
@@ -52,25 +80,17 @@ function mapToLayers(pois: POI[], categories: PoiCategory[]) {
           features: pois
             .filter((poi) => poi.poiCategory === category.sourceId)
             .map((poi) => {
-              let homePage: string = ''
-              let tel: string = ''
-              poi.details.forEach((detail) => {
-                if (detail.type === 'url') homePage = detail.value
-                if (detail.type === 'tel') tel = detail.value
-              })
               return {
                 type: 'Feature',
                 properties: {
                   symbolPath: category.symbolPath,
                   symbolName: category.symbolName,
                   symbolMimetype: category.symbolMimetype,
-                  address: poi.address,
-                  city: poi.city,
-                  district: poi.district,
-                  name: poi.name,
-                  zip: poi.zip,
-                  homePage: homePage,
-                  tel: tel
+                  iconPath: category.iconPath,
+                  iconName: category.iconName,
+                  iconMimetype: category.iconMimetype,
+                  coordinates: [poi.geopoint.longitude, poi.geopoint.latitude],
+                  poi: poi
                 },
                 geometry: {
                   type: 'Point',
@@ -86,6 +106,37 @@ function mapToLayers(pois: POI[], categories: PoiCategory[]) {
   return res
 }
 
+function drawLine(points: any[], color: string = environment.primaryColor || '#004373') {
+  if (points?.length) {
+    for (let i = 0; i < points.length; i++) {
+      points[i] = olProj.transform([points[i][1], points[i][0]], 'EPSG:4326', 'EPSG:25832')
+    }
+
+    const featureLine = new ol.Feature(new olGeom.LineString(points))
+
+    const vectorLine = new olSource.Vector({})
+    vectorLine.addFeature(featureLine)
+
+    const vectorLineLayer = new olLayer.Vector({
+      source: vectorLine,
+      style: new olStyle.Style({
+        fill: new olStyle.Fill({ color: color }),
+        stroke: new olStyle.Stroke({ color: color, width: 5 })
+      })
+    })
+
+    vectorLineLayer.setProperties({ name: 'trip' })
+
+    mpapi.map.getLayers().forEach((layer) => {
+      if (layer.get('id') !== 'base') {
+        layer.setZIndex(1000)
+      }
+    })
+
+    mpapi.map.addLayer(vectorLineLayer)
+  }
+}
+
 export default function MapPanel({
   preSelected
 }: {
@@ -93,12 +144,18 @@ export default function MapPanel({
 }): React.JSX.Element {
   const [first, setFirst] = useState(true)
   const [information, setInformation] = useState(undefined)
+  const [trip, setTrip] = useState(undefined)
+
   const categories = useLiveQuery(async () => {
-    return await db.poiCategories.toArray()
+    return db.poiCategories.toArray()
   })
 
   const pois = useLiveQuery(async () => {
-    return await db.pois.toArray()
+    return db.pois.toArray()
+  })
+
+  const screen = useLiveQuery(async () => {
+    return db.screen.toCollection().first()
   })
 
   const [activeLayers, setActiveLayers] = useState<string[]>([])
@@ -115,8 +172,74 @@ export default function MapPanel({
     }
   }
 
+  function getTrip(
+    from: { latitude: number; longitude: number },
+    to: { latitude: number; longitude: number }
+  ): Promise<[{ trips: [{ coords: [number]; interchange: { coords: [number] } }] }]> {
+    return new Promise((resolve, reject) => {
+      Promise.all([
+        Parse.Cloud.run(
+          'pt-stop-nearby',
+          { lat: from.latitude, lon: from.longitude },
+          { useMasterKey: true }
+        ),
+        Parse.Cloud.run(
+          'pt-stop-nearby',
+          { lat: to.latitude, lon: to.longitude },
+          { useMasterKey: true }
+        )
+      ]).then((a) => {
+        Parse.Cloud.run(
+          'pt-trip',
+          {
+            from: {
+              geoPoint: '[' + from.latitude + ', ' + from.longitude + ']',
+              stop: a[0].stops[0].properties.stopId
+            },
+            to: {
+              geoPoint: '[' + to.latitude + ', ' + to.longitude + ']',
+              stop: a[1].stops[0].properties.stopId
+            }
+          },
+          { useMasterKey: true }
+        ).then((a) => {
+          resolve(a)
+        })
+      })
+    })
+  }
+
   useEffect(() => {
-    // eslint-disable-next-line
+    if (screen) {
+      const center = olProj.transform(
+        [screen.location.longitude, screen.location.latitude],
+        'EPSG:4326',
+        'EPSG:25832'
+      )
+
+      mpapi.map.getView().setCenter(center)
+
+      const featureLine = new ol.Feature(new olGeom.Point(center))
+      featureLine.setProperties({ poi: { details: [{ title: 'Sie Sind Hier' }] } })
+
+      const vectorLine = new olSource.Vector({})
+      vectorLine.addFeature(featureLine)
+
+      const vectorLineLayer = new olLayer.Vector({
+        properties: { name: 'asdf' },
+        source: vectorLine,
+        style: new olStyle.Style({
+          image: new olStyle.Icon({
+            src: 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png',
+            scale: 0.09
+          })
+        })
+      })
+      mpapi.map.addLayer(vectorLineLayer)
+    }
+  }, [screen])
+
+  useEffect(() => {
     document.getElementById(portalConfig.target).innerHTML = ''
 
     if (categories && pois) {
@@ -150,60 +273,167 @@ export default function MapPanel({
       const coordinate = evt.coordinate
       const tmpContainer = document.getElementById('popupDiv')
       overlay.setElement(tmpContainer)
+      overlay.setPositioning('center-center')
       overlay.setPosition(coordinate)
       mpapi.map.addOverlay(overlay)
 
       const feature = mpapi.map.forEachFeatureAtPixel(evt.pixel, (feature) => {
         return feature
       })
-      if (feature) {
+
+      if (feature?.values_?.poi) {
         setInformation(feature.values_)
+        mpapi.map.getView().animate({
+          center: coordinate,
+          duration: 800
+        })
       } else {
         setInformation(undefined)
       }
     })
   }, [categories, pois])
 
+  function tripTo(longitude: number, latitude: number) {
+    getTrip(
+      { longitude: screen.location.longitude, latitude: screen.location.latitude },
+      {
+        longitude: longitude,
+        latitude: latitude
+      }
+    ).then((a) => {
+      for (let i = 0; i <= a[0]?.trips.length; i++) {
+        setTrip(a[0])
+        drawLine(a[0]?.trips[i]?.interchange?.coords, 'red')
+        drawLine(
+          a[0]?.trips[i]?.coords,
+          i % 2 === 0
+            ? environment.primaryColor || '#004373'
+            : environment.secondaryColor || '#0a7ac9'
+        )
+      }
+    })
+  }
+
   return (
     <>
-      <div id="popupDiv" style={information ? { display: 'block' } : { display: 'none' }}>
-        <BubbleInfo information={information} />
+      <div
+        id="popupDiv"
+        className="w-fit min-w-[18rem] max-w-[36rem] grid"
+        style={{ gridTemplateColumns: 'minmax(0, 4fr) minmax(0, 1fr)' }}
+      >
+        {information && <BubbleInfo tripTo={tripTo.bind(this)} information={information} />}
       </div>
 
-      <div className="h-full" id="map-div-id"></div>
-      {categories && (
-        <div className="absolute bg-primary-color bg-opacity-90 bottom-0 w-full text-on-primary-color py-[1%] h-40 flex">
-          {categories.map((category, index) => {
-            if (category.showCategory === 'true') {
-              return (
-                <div
-                  key={index}
-                  className="flex flex-col items-center justify-center text-center w-full"
-                  onClick={() => {
-                    switchLayer(category.name), setInformation(undefined)
+      <div
+        className="w-screen h-full grid"
+        style={{ gridTemplateRows: 'auto minmax(0, min-content)' }}
+      >
+        <div
+          className="h-full w-full grid grid-flow-col"
+          style={{ gridTemplateColumns: '10fr auto' }}
+        >
+          <div className="h-full" id="map-div-id"></div>
+          {trip && (
+            <div className="flex flex-col p-10 bg-primary-color text-on-primary-color h-full w-[25rem] text-2xl">
+              <div className="w-full h-min flex justify-between">
+                <span className="text-4xl text-secondary-color font-bold">Reiseinfos:</span>
+                <button
+                  onMouseDown={() => {
+                    mpapi.map
+                      .getLayers()
+                      .getArray()
+                      .filter((el) => el.get('name') === 'trip')
+                      .forEach((layer) => {
+                        mpapi.map.removeLayer(layer)
+                      })
+                    setTrip(undefined)
+                    setInformation(undefined)
                   }}
+                  className="bg-gray-500 text-on-secondary-color bg-opacity-50 aspect-square h-full rounded-md"
                 >
-                  <img
-                    draggable="false"
-                    className={
-                      activeLayers.includes(category.name)
-                        ? 'rounded-full bg-secondary-color transition-all duration-300 aspect-square mb-4 h-24'
-                        : 'rounded-full bg-white transition-all duration-300 aspect-square mb-4 opacity-70 h-24'
-                    }
-                    src={category.iconPath + '/' + category.iconName + category.iconMimetype}
-                  ></img>
-                  <div>
-                    <div className="text-xl inline-block whitespace-nowrap">
-                      {category.mapTitle}&nbsp;
-                      <span className="text-secondary-color">{'>'}</span>
-                    </div>
-                  </div>
-                </div>
-              )
-            }
-          })}
+                  <Close
+                    width="100%"
+                    height="2rem"
+                    fill={environment.onPrimaryColor || '#FFFFFF'}
+                  ></Close>
+                </button>
+              </div>
+              <div className="h-full grid grid-flow-row">
+                {trip.trips.map((t, index) => {
+                  return (
+                    <>
+                      <div className="w-full flex flex-row justify-between items-center">
+                        <img
+                          width="12%"
+                          src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png"
+                        ></img>
+                        {t.origin.name}
+                      </div>
+                      <div className="py-10 px-5 h-full border-l-4 w-full ml-[1.2rem] flex flex-col items-start justify-around">
+                        {t.duration && (
+                          <span className="text-lg">Dauer: {Math.round(t.duration / 60)} Min.</span>
+                        )}
+
+                        {t.transportation && (
+                          <span className="text-lg">{t.transportation.name}</span>
+                        )}
+                      </div>
+
+                      {trip.trips.length === index + 1 ? (
+                        <div className="w-full flex flex-row justify-between items-center">
+                          <img
+                            width="12%"
+                            src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png"
+                          ></img>
+                          {t.destination?.name}
+                        </div>
+                      ) : (
+                        <></>
+                      )}
+                    </>
+                  )
+                })}
+              </div>
+            </div>
+          )}
         </div>
-      )}
+
+        {categories && (
+          <div className="w-full h-full bg-primary-color flex overflow-x-scroll scrollbar-hide justify-around text-on-primary-color">
+            <div className="h-full grid grid-flow-col items-around w-full py-4 overflow-normal">
+              {categories.map((category, index) => {
+                if (category.showCategory === 'true') {
+                  return (
+                    <div
+                      key={index}
+                      className="flex flex-col items-center justify-center text-center w-40"
+                      onMouseDown={() => {
+                        switchLayer(category.name), setInformation(undefined)
+                      }}
+                    >
+                      <img
+                        draggable="false"
+                        className={
+                          activeLayers.includes(category.name)
+                            ? 'rounded-full bg-secondary-color transition-all duration-300 aspect-square mb-4 h-24'
+                            : 'rounded-full bg-background-color transition-all duration-300 aspect-square mb-4 opacity-70 h-24'
+                        }
+                        src={category.iconPath + '/' + category.iconName + category.iconMimetype}
+                      ></img>
+                      <div>
+                        <div className="text-xl inline-block whitespace-nowrap w-full">
+                          {category.mapTitle}&nbsp;
+                          <span className="text-secondary-color">{'>'}</span>
+                        </div>
+                      </div>
+                    </div>
+                  )
+                }
+              })}
+            </div>
+          </div>
+        )}
+      </div>
     </>
   )
 }
