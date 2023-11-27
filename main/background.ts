5a70c4051f272c41af30248c57f136901ab98f66
Jakob Windoffer <j.windoffer@nedeco.de>
2023-11-27 13:44:26 +0100

    feat(updated): added auto-updater functionality

--- main/background.ts
+++ main/background.ts
@@ -1,6 +1,7 @@
 import { app } from 'electron'
 import serve from 'electron-serve'
 import { createWindow } from './helpers'
+import { autoUpdater } from 'electron-updater'
 
 const isProd: boolean = process.env.NODE_ENV === 'production'
 
@@ -28,6 +29,38 @@ if (isProd) {
     const userPath = app.getPath('userData').replaceAll('\\', '/')
     mainWindow.webContents.executeJavaScript(`localStorage.setItem("path", "${userPath}")`, true)
     mainWindow.webContents.executeJavaScript(`localStorage.setItem("mode", "prod")`, true)
+
+    autoUpdater.on('checking-for-update', () => {
+      mainWindow.webContents.executeJavaScript(`console.log('Checking for update...')`)
+    })
+    autoUpdater.on('update-available', (info) => {
+      mainWindow.webContents.executeJavaScript(`console.log('Update available.')`)
+    })
+    autoUpdater.on('update-not-available', (info) => {
+      mainWindow.webContents.executeJavaScript(`console.log('Update not available.')`)
+    })
+    autoUpdater.on('error', (err) => {
+      const a = 'Error in auto-updater, ' + JSON.stringify(err)
+      mainWindow.webContents.executeJavaScript(`console.log('${a}')`)
+    })
+    autoUpdater.on('download-progress', (progressObj) => {
+      let log_message = 'Download speed: ' + progressObj.bytesPerSecond
+      log_message = log_message + ' - Downloaded ' + progressObj.percent + '%'
+      log_message = log_message + ' (' + progressObj.transferred + '/' + progressObj.total + ')'
+      mainWindow.webContents.executeJavaScript(`console.log(${log_message})`)
+    })
+    autoUpdater.on('update-downloaded', (info) => {
+      mainWindow.webContents.executeJavaScript(`console.log('Update downloaded')`)
+    })
+
+    autoUpdater.on('update-downloaded', () => {
+      autoUpdater.quitAndInstall()
+    })
+
+    // Check for updates every 6 hours
+    setInterval(() => {
+      autoUpdater.checkForUpdates()
+    }, /*21600000*/ 80000)
   } else {
     const port = process.argv[2]
     await mainWindow.loadURL(`http://localhost:${port}/dashboard`)
