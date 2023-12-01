c70738731b6d28c14d27a97b8f81a34cb153f1b0
Jakob Windoffer <j.windoffer@nedeco.de>
2023-12-01 11:36:22 +0100

    fix: fix dashboard bug

--- renderer/pages/dashboard.tsx
+++ renderer/pages/dashboard.tsx
@@ -9,7 +9,7 @@ interface Props {
   isParseOnline: boolean
 }
 // eslint-disable-next-line sonarjs/cognitive-complexity
-//aa
+//aaa
 const Dashboard = ({ isParseOnline }: Props) => {
   const screen = useLiveQuery(async () => {
     return db.screen.toCollection().first()
