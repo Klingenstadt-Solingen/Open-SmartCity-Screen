feb2ef949c9abd6c456a981305078786980c0815
Jakob Windoffer <j.windoffer@nedeco.de>
2024-09-04 10:10:46 +0200

    feat(start_script): add start_windows.bat

--- renderer/environment.ts
+++ renderer/environment.ts
@@ -6,11 +6,6 @@ export const environment = {
   onSecondaryColor: null,
   backgroundColor: null,
   backgroundColorDark: null,
-  // parseUrl: 'http://localhost:1337/parse',
-  // parseAppId: '***REMOVED***',
-  // parseClientKey: 'myclientkey',
-  // parseJavascriptKey: '***REMOVED***',
-
   parseUrl: process.env.NEXT_PUBLIC_PARSE_URL,
   parseAppId: process.env.NEXT_PUBLIC_PARSE_APP_ID,
   parseJavascriptKey: process.env.NEXT_PUBLIC_PARSE_JAVASCRIPT_KEY,
